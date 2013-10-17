79811462b262e8a198ad585bbc81b433282b08ec
planewalker <stefan.kovachev@gmail.com>
2013-10-17 17:07:00 +0300

    Added a command class that can run continuously

---
+++ src/Skovachev/Lacore/ContinuousCommand.php
@@ -0,0 +1,36 @@
+<?php namespace Youtubedl\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+abstract class ContinuousCommand extends Command {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $options = array(
+            array('continuous', null, InputOption::VALUE_OPTIONAL, 'Run the command as a continuous cycle', 60),
+        );
+        call_user_func_array(array($this, 'addOption'), $options);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $continuous = $this->option('continuous');
+        if (!is_null($continuous))
+        {
+            while (true)
+            {
+                $this->fire();
+                sleep($continuous);
+            }
+        }
+        else
+        {
+            return $this->fire();
+        }
+    }
+}
